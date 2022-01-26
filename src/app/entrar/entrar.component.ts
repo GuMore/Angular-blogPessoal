d63c9323d981041e6e4f6994fd2eed3096d0b578
GuMore <gustavoglm1405@gmail.com>
2022-01-26 19:57:56 -0300

    Adicionando inicio e consumo de API ao meu Blog Pessoal

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -33,8 +33,8 @@ export class EntrarComponent implements OnInit {
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
+      console.log(environment)
 
-      this.userLogin.foto
 
       this.router.navigate(['/inicio'])
     }, erro =>{
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment.prod';
 import { UserLogin } from './../model/UserLogin';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -14,10 +15,19 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', UserLogin)
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+    return ok
   }
 }
